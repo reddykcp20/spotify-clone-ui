9cae6e7e367b9abb4df5616c1f856d2ec6209e1c
reddykcp20 <reddykcp20@gmail.com>
2023-02-27 13:23:13 +0530

    UI changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Spotify - Web Player';
+
+  constructor(private _router: Router) {}
+
+  ngOnInit() {
+    if (localStorage.getItem('auth') === 'true') {
+      this._router.navigateByUrl('/home');
+    }
+  }
 }
